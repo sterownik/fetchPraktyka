00220300dc502f0f5bdd6ab69a0154192ec1dbcf
Wojciech Gołda <w.golda@praktyki.ideo.pl>
2020-09-21 11:41:50 +0200

    poprawa stylowania vol2

--- src/components/AddPerson.js
+++ src/components/AddPerson.js
@@ -3,7 +3,14 @@ import React from "react";
 const AddPerson = (props) => {
   return (
     <button
-      style={{ margin: "50px", height: "60px", width: "120px" }}
+      style={{
+        margin: "50px",
+        height: "60px",
+        width: "120px",
+        backgroundColor: "#1d3557",
+        border: "null",
+        color: "#f1faee",
+      }}
       onClick={props.onclick}
     >
       Dodaj osobę
