6d7c35bf70d686801d9f1839dccdfd5eea1d9637
Wojciech Gołda <w.golda@praktyki.ideo.pl>
2020-09-21 11:19:10 +0200

    Updates

---
+++ precache-manifest.ae78b84ae1b2b517ff8f8b97099b0d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f67965c6d67c184d624705095c116a6",
+    "url": "/fetchPraktyka/index.html"
+  },
+  {
+    "revision": "7a68639c37220b96e746",
+    "url": "/fetchPraktyka/static/css/main.095cebe2.chunk.css"
+  },
+  {
+    "revision": "2cc2e00e124d1b0a4c6e",
+    "url": "/fetchPraktyka/static/js/2.ca6eab21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fetchPraktyka/static/js/2.ca6eab21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a68639c37220b96e746",
+    "url": "/fetchPraktyka/static/js/main.f97089ad.chunk.js"
+  },
+  {
+    "revision": "6fd4d5bcef88218366df",
+    "url": "/fetchPraktyka/static/js/runtime-main.47899628.js"
+  }
+]);
